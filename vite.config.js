149e4c5c271d8166c16811380f4e43bc4632d862
Josiah Smith <jcsmithf22@gmail.com>
2024-09-08 17:21:18 -0500

    complete switch to vite

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,18 @@ import symfonyPlugin from "vite-plugin-symfony";
 // import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        /* react(), // if you're using React */
-        symfonyPlugin(),
-    ],
-    build: {
-        rollupOptions: {
-            input: {
-                app: "./assets/app.js"
-            },
-        }
-    },
+  plugins: [
+    /* react(), // if you're using React */
+    symfonyPlugin({
+      stimulus: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./assets/app.js",
+        css: "./assets/styles/app.css"
+      },
+    }
+  },
 });
