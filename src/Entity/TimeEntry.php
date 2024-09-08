c72aacefcb7c639aa175e2a4bc72b35091ec2754
Josiah Smith <jcsmithf22@gmail.com>
2024-09-08 01:46:08 -0500

    initial commit

---
+++ src/Entity/TimeEntry.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TimeEntryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TimeEntryRepository::class)]
+class TimeEntry
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
