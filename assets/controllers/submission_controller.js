7dc142d6427661f4bbc52b6a1e8578e8043e5c19
Josiah Smith <jcsmithf22@gmail.com>
2024-09-09 18:42:36 -0500

    Add more form features

--- assets/controllers/submission_controller.js
+++ assets/controllers/submission_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
 
   addCollectionElement(event) {
     const item = document.createElement('li');
-    const classes = "w-full grid gap-3 md:grid-cols-4".split(' ');
+    const classes = "w-full grid gap-3 md:grid-cols-4 p-4 border-t border-gray-300 -mb-3".split(' ');
     item.classList.add(...classes)
     item.innerHTML = this.prototypeValue.replace(/__name__/g, this.indexValue);
     this.collectionContainerTarget.appendChild(item);
