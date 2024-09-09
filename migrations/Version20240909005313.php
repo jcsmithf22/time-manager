7a976003334cf80dfc18da05d20d90c832cc9057
Josiah Smith <jcsmithf22@gmail.com>
2024-09-08 19:56:09 -0500

    Add mailer

--- migrations/Version20240909005313.php
+++ migrations/Version20240909005313.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240909004440 extends AbstractMigration
+final class Version20240909005313 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,6 @@ final class Version20240909004440 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE time_entry_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE time_entry (id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
         $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:ulid)\'');
@@ -31,8 +29,6 @@ final class Version20240909004440 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE time_entry_id_seq CASCADE');
-        $this->addSql('DROP TABLE time_entry');
         $this->addSql('DROP TABLE "user"');
     }
 }
