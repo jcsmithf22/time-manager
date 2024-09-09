5f0d9bde059b5fadcac6dd59ed6d77b7776bafab
Josiah Smith <jcsmithf22@gmail.com>
2024-09-08 20:55:24 -0500

    Disable email confirmation in user registration
    Commented out the email confirmation functionality in the RegistrationController. This change allows users to register without needing to verify their email addresses via a confirmation email.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,13 +43,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@timemanager.com', 'Time Manager'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+//                (new TemplatedEmail())
+//                    ->from(new Address('mailer@timemanager.com', 'Time Manager'))
+//                    ->to((string) $user->getEmail())
+//                    ->subject('Please Confirm your Email')
+//                    ->htmlTemplate('registration/confirmation_email.html.twig')
+//            );
 
             // do anything else you need here, like send an email
 
