6065530abdc0938ad8b2a797ccb6a25f9210839f
Josiah Smith <jcsmithf22@gmail.com>
2024-09-11 09:51:23 -0500

    fix password reset

--- src/Repository/ResetPasswordRequestRepository.php
+++ src/Repository/ResetPasswordRequestRepository.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
+use DateTimeInterface;
 
 /**
  * @extends ServiceEntityRepository<ResetPasswordRequest>
@@ -28,5 +29,45 @@ class ResetPasswordRequestRepository extends ServiceEntityRepository implements 
     public function createResetPasswordRequest(object $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken): ResetPasswordRequestInterface
     {
         return new ResetPasswordRequest($user, $expiresAt, $selector, $hashedToken);
+    }
+
+    /**
+     * Override trait method because of using ulid for user ID.
+     *
+     * @param User $user
+     */
+    public function getMostRecentNonExpiredRequestDate(object $user): ?DateTimeInterface
+    {
+        // Normally there is only 1 max request per use, but written to be flexible
+        /** @var ResetPasswordRequestInterface $resetPasswordRequest */
+        $resetPasswordRequest = $this->createQueryBuilder('t')
+            ->where('t.user = :user')
+            ->setParameter('user', $user->getId(), 'ulid')
+            ->orderBy('t.requestedAt', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneorNullResult();
+        if (null !== $resetPasswordRequest && !$resetPasswordRequest->isExpired()) {
+            return $resetPasswordRequest->getRequestedAt();
+        }
+
+        return null;
+    }
+
+    /**
+     * Override trait method because of using ulid for user ID.
+     */
+    public function removeResetPasswordRequest(ResetPasswordRequestInterface $resetPasswordRequest): void
+    {
+        /** @var User $user */
+        $user = $resetPasswordRequest->getUser();
+
+        $this->createQueryBuilder('t')
+            ->delete()
+            ->where('t.user = :user')
+            ->setParameter('user', $user->getId(), 'ulid')
+            ->getQuery()
+            ->execute()
+        ;
     }
 }
